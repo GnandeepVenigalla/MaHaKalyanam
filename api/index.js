81cabcc1d4e8fe7ae6e8f6a4d82aeca748736ca8
GnandeepVenigalla <gnandeep.venigalla.5@gmail.com>
2026-06-06 13:15:46 -0400

    dmin fix

--- api/index.js
+++ api/index.js
@@ -3,9 +3,6 @@
 // All /api/* requests are handled by this function
 // ============================================================
 
-import dotenv from 'dotenv';
-dotenv.config({ path: new URL('../server/.env', import.meta.url).pathname });
-
 import app from '../server/index.js';
 
 export default app;
